0f9aa857c8c162c701b89c24983581cbcfa248b2
PhatITpromax <phatvo779@gmail.com>
2025-08-02 19:26:41 +0700

    ok

---
+++ src/data/aboutData.jsx
@@ -0,0 +1,36 @@
+const aboutData = {
+    title: "About Me",
+    subtitle: "Discover my journey, passions, and the story behind my work",
+    image: "/assets/profile.jpg",
+
+    biodata: [
+        { label: "Name", value: "Hizkia Siahaan", icon: "bx bx-id-card" },
+        { label: "Date of Birth", value: "December,02-2004", icon: "bx bx-calendar" },
+        { label: "Place of Birth", value: "Medan, Indonesia", icon: "bx bx-map" },
+        { label: "Email", value: "siahaanhizkia548@gmail.com", icon: "bx bx-envelope" },
+        { label: "Phone", value: "+62 895 1014 4492", icon: "bx bx-phone" },
+        { label: "Education", value: "Universitas Sumatera Utara", icon: "bx bx-book" },
+        { label: "GPA", value: "3.55 / 4.00", icon: "bx bx-award" },
+    ],
+
+    resume: {
+        label: "Download My Resume",
+        href: null,
+        icon: "bx bx-download",
+    },
+
+    aboutNarrative: {
+        whoAmI: {
+            text: `I'm a frontend developer who crafts responsive, interactive, and clean UIs. With a background in Informatics Engineering, I blend design sense with technical logic.`,
+            icon: "bx-info-circle"
+        },
+        approach: {
+            text: `I focus on user-centered design, performance, and accessibility   always evolving with modern tech to deliver seamless digital experiences.`,
+            icon: "bx-bulb"
+        }
+    }
+
+
+};
+
+export default aboutData;
